c7ac7d4f45f12243f3e54dd6b2acb4690c192246
Josef Tinagan <tinaganjosefperry@gmail.com>
2026-08-20 19:11:31 +0800

    feat: implement basic validation for coordinate checking

--- apps/web/src/pages/picture-specific/ui/SpecificPicturePage.jsx
+++ apps/web/src/pages/picture-specific/ui/SpecificPicturePage.jsx
@@ -6,8 +6,22 @@ export default function SpecificPicturePage(){
 
   const [userClickCoords, setUserClickCoords] = useState({x: 0, y: 0});
   const [showPopup, setShowPopup] = useState(false);
+  const [currentUserChoice, setCurrentUserChoice] = useState(null);
 
   const imgRef = useRef(null);
+
+  // Testing, (middle (screw) of a blue cutter)
+  const defaultImageSize = { width: 1600, height: 1403}
+  const testTagCoords = { x: 1314, y: 755}
+  const testTagPercentage = { x: (testTagCoords.x / defaultImageSize.width) * 100, y: (testTagCoords.y / defaultImageSize.height) * 100}
+
+  console.log("Test tag console:");
+  console.log(testTagPercentage.x);
+  console.log(testTagPercentage.y)
+
+  // Testing normalizing of coordinates
+  const [userClickCoordsDecimal, setUserClickCoordsDecimal] = useState({x: 0, y: 0});
+  // const [boundingRect, setBoundingRect] = useState({});
 
   const imageClickHandler = (e) => {
     if(imgRef.current){
@@ -15,7 +29,7 @@ export default function SpecificPicturePage(){
       console.log(rect);
       console.log(rect.width);
       console.log(rect.height);
-    }
+    
 
     // Getting coordinates of the click area
     console.log("Clicked Coordinates");
@@ -24,27 +38,87 @@ export default function SpecificPicturePage(){
     setUserClickCoords({x: e.pageX, y: e.pageY});
     setShowPopup(prev => !prev);
 
+    // Testing normalizing of coordinates
+    setUserClickCoordsDecimal({x: (e.pageX / rect.width), y: (e.pageY / rect.height) });
+    
+    }
+  }
+
+  const testTagEventHandler = (e) => {
+    console.log("Test tag event handler triggered...");
+    imageClickHandler(e);
+  }
+
+  const selectOnChangeHandler = (e) => {
+    console.log("Selected: ", e.target.value);
+    validate(e.target.value);
+  }
+
+
+  const validate = (userChoice) => {
+    console.log("Validating...");
+    console.log(userChoice);
+
+    console.log("User clicked coordinates:");
+    console.log(`X: ${userClickCoords.x}, Y: ${userClickCoords.y}`);
+
+    const targetingBoxCoords = {
+      topLeft: { x: userClickCoords.x - 10, y: userClickCoords.y - 10},
+      topRight: { x: userClickCoords.x + 10, y: userClickCoords.y - 10},
+      bottomLeft: {x: userClickCoords.x - 10, y: userClickCoords.y + 10},
+      bottomRight: {x: userClickCoords.x + 10, y: userClickCoords.y + 10}
+
+    }
+
+    console.log(`Top Left: x: ${targetingBoxCoords.topLeft.x}, y: ${targetingBoxCoords.topLeft.y}`);
+    console.log(`Top Right: x: ${targetingBoxCoords.topRight.x}, y: ${targetingBoxCoords.topRight.y}`);
+
+    console.log("Iterating targeting box object:");
+    for(const [key, value] of Object.entries(targetingBoxCoords)) {
+      console.log(`Key: ${key}`);
+      for(const [key1, value1] of Object.entries(value)){
+        console.log(`${key1}: ${value1}`);
+      }
+    }
+
+    // The actual validation of coordinates
+
+    let hit = false;
+    console.log(`${targetingBoxCoords.topLeft.x} < ${testTagCoords.x} < ${targetingBoxCoords.topRight.x}`)
+    console.log(`${targetingBoxCoords.topLeft.y} < ${testTagCoords.y} < ${targetingBoxCoords.topRight.y}`)  
+    if((targetingBoxCoords.topLeft.x <= testTagCoords.x && testTagCoords.x <= targetingBoxCoords.topRight.x) &&
+      (targetingBoxCoords.bottomLeft.x <= testTagCoords.x && testTagCoords.x <= targetingBoxCoords.bottomRight.x)
+      ){
+      console.log("HIT!");
+    }
+    if(((targetingBoxCoords.topLeft.y <= testTagCoords.y && testTagCoords.y <= targetingBoxCoords.topRight.y)) &&
+        (targetingBoxCoords.bottomLeft.y <= testTagCoords.y && testTagCoords.y <= targetingBoxCoords.bottomLeft.y)){
+      console.log("Hit 2!!");      
+    }
+    // if(targetingBoxCoords.topLeft.x < testTagCoords.x &&  testTagCoords.x < targetingBoxCoords.topRight.x){
+    //   console.log("HIT!!!!")
+    // }
   }
 
   return(
     <div>
       <div className={styles.playArea}>
         <div className={styles.imageContainer}>
-          <div 
-            className={styles.popUpBox}
-            style={{ visibility: showPopup ? 'visible' : 'hidden', top: userClickCoords.y, left: userClickCoords.x}} 
-          >
+          <div className={styles.popUpBox} style={{ visibility: showPopup ? 'visible' : 'hidden', top: userClickCoords.y, left: userClickCoords.x}}>
             <div className={styles.targetingBox}/>
-            <div>
+            <div className={styles.selectionContainer}>
               <label htmlFor="possibleElement">Which element is here?</label>
-              <select name="possibleElement" id="possibleElement">
+              <select name="possibleElement" id="possibleElement" onChange={selectOnChangeHandler}>
                 <option value="">Choose...</option>
                 <option value="lightbulb">Lightbulb</option>
                 <option value="cellphone">Cellphone</option>
                 <option value="sunglasses">Sunglasses</option>
               </select>
             </div>
+            <div className={styles.errorMsg}>Not quite!</div>
+            <div className={styles.successMsg}>You got it!</div>
           </div>
+          <div className={styles.testTag} style={{ top: `${testTagPercentage.y}%`, left: `${testTagPercentage.x}%`}} onClick={testTagEventHandler}></div>
           <img
             ref={imgRef}
             src="https://media.istockphoto.com/id/171114507/photo/junk-in-a-drawer.jpg?s=2048x2048&w=is&k=20&c=xMyVLa4tCppHkovgkUMYtNEVXGEKRv141LUlrU5S4zk=" 
@@ -59,6 +133,7 @@ export default function SpecificPicturePage(){
         Test 
         <p>User Click Coordinates: X: {userClickCoords.x}, Y: {userClickCoords.y}</p>
         <p>Showing Popup? {showPopup ? "true" : "false"}</p>
+        <p>User Click Coordinates: X: {userClickCoordsDecimal.x}, Y: {userClickCoordsDecimal.y}</p>
       </div>
     </div>
   )
